1b4a48545b9421e067fe5a344ff761e3c93cd6d6
Christopher Massey <moonix.uk@gmail.com>
2020-02-10 21:53:35 +0000

    migrate caps errors and composer update

--- database/migrations/2019_01_30_145217_create_locations_table.php
+++ database/migrations/2019_01_30_145217_create_locations_table.php
@@ -13,7 +13,7 @@ class CreateLocationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('Locations', function (Blueprint $table) {
+        Schema::create('locations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('parent_id')->unsigned()->nullable();
             $table->integer('depth');
